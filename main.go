6b8bcde4c11f377d588ddbdf3496a30e01a8cfba
Fyodor Bashlakov <feiaoduoit@gmail.com>
2024-06-04 09:39:41 +0700

    add chi

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hellow world")
-
 	// loads .env file
 	godotenv.Load(".env")
 
@@ -21,5 +21,24 @@ func main() {
 		log.Fatal("Port is not found in the enviroment")
 	}
 
-	fmt.Println("Port:", portString)
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+	log.Printf("Server staring on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
